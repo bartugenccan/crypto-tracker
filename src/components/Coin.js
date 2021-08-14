98f21b84878ad37aeb8368d6202457c99031716a
bartugenccan <bartugenccan@gmail.com>
2021-08-14 13:25:07 +0300

    image

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -33,6 +33,16 @@ const Coin = ({
           <p className="coin-marketcap">Cap: $ {marketcap.toLocaleString()}</p>
         </div>
       </div>
+      <h1>
+        This is a Crypto Tracker app that shows current Coin status. I used
+        CoinGecko API to render coins.
+      </h1>
+      <h3>
+        Demo :{" "}
+        <a href="https://crypto-tracker-epmjn00l8-bartugenccan.vercel.app/">
+          https://crypto-tracker-epmjn00l8-bartugenccan.vercel.app/
+        </a>
+      </h3>
     </div>
   );
 };
